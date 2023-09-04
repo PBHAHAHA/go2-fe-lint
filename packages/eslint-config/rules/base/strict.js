2f98eab33c730205194155db2361c59db8f35365
pub <pubing@go2.cn>
2023-09-04 16:49:57 +0800

    style: change eslint-config style

--- packages/eslint-config/rules/base/strict.js
+++ packages/eslint-config/rules/base/strict.js
@@ -7,8 +7,8 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = {
-    rules: {
-      // 不限制严格模式的使用
-      strict: 'off',
-    },
+  rules: {
+    // 不限制严格模式的使用
+    strict: 'off',
+  },
 };
--- packages/eslint-config/rules/es5.js
+++ packages/eslint-config/rules/es5.js
@@ -2,12 +2,12 @@
  * es5的 eslint 配置
  */
 module.exports = {
-    extends: [
-      './rules/base/best-practices',
-      './rules/base/possible-errors',
-      './rules/base/style',
-      './rules/base/variables',
-      './rules/es5',
-    ].map(require.resolve),
-    root: true,
-  };
+  extends: [
+    './rules/base/best-practices',
+    './rules/base/possible-errors',
+    './rules/base/style',
+    './rules/base/variables',
+    './rules/es5',
+  ].map(require.resolve),
+  root: true,
+};
