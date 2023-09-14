533238e2f3d9dda8f21b2fb2d8b0c3471e465da7
pub <pubing@go2.cn>
2023-09-14 10:04:46 +0800

    feat: static vue config eslintrc.js

--- packages/eslint-config/typescript/vue.js
+++ packages/eslint-config/typescript/vue.js
@@ -7,12 +7,12 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = {
-    extends: [
-      './index',
-      '../rules/vue',
-    ].map(require.resolve),
-    parserOptions: {
-      // https://github.com/mysticatea/vue-eslint-parser#parseroptionsparser
-      parser: '@typescript-eslint/parser',
-    },
-  };
+  extends: [
+    './index',
+    '../rules/vue',
+  ].map(require.resolve),
+  parserOptions: {
+    // https://github.com/mysticatea/vue-eslint-parser#parseroptionsparser
+    parser: '@typescript-eslint/parser',
+  },
+};
